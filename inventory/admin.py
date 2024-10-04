a743cddfc43b16f0f015d9c992a81632f525725c
million-tadele <163776419+million-tadele@users.noreply.github.com>
2024-10-04 21:44:29 +0300

    Delete inventory directory

--- inventory/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import InventoryItem, Category, Supplier
-
-# Register your models here.
-admin.site.register(InventoryItem)
-admin.site.register(Category)
-admin.site.register(Supplier)
